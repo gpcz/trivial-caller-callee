f0aa83207759d6f2ad378bf8450b47161e72790b
Greg Czerniak <gregczrk@gmail.com>
2024-09-02 06:15:50 -0400

    Added workflow for CCL

---
+++ gh-workflow-runtests.lisp
@@ -0,0 +1,12 @@
+(require 'asdf)
+
+(asdf:initialize-source-registry
+  (list
+   :source-registry
+   (list :tree (merge-pathnames
+                "common-lisp"
+                (uiop:pathname-parent-directory-pathname
+                 (uiop/os:getcwd))))
+   :inherit-configuration))
+
+(asdf:test-system :trivial-caller-callee)
